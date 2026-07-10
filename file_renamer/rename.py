937d5d4e9624c06920a9795ca3867ebe19b4849d
Juned1306 <shaikhjuned3161@gmail.com>
2026-07-10 17:49:32 +0530

    Implement file renaming functionality
    The script changes the working directory and renames files by extracting and formatting parts of their names.

--- file_renamer/rename.py
+++ file_renamer/rename.py
@@ -1,1 +1,25 @@
+import os
 
+os.chdir('D:\\Python\\File Operation\\Renaming Mult Filles')
+
+
+for f in os.listdir():
+    #print(f)
+    
+    f_name,f_extention=os.path.splitext(f)
+    f_title,f_desc,f_num=f_name.split('-')
+    f_title=f_title.strip()
+    f_desc=f_desc.strip()
+    f_num=f_num.strip()[1:].zfill(2)
+    print(f_num,f_title,f_desc)
+    print('------------------------------')
+    new_name=f"{f_num}-{f_title}-{f_desc}{f_extention}"
+    print(new_name)
+    print('------------------------------')
+    os.rename(f,new_name)
+    
+    
+    
+    
+
+    
